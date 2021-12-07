b1e76e46207e308a71fb834c915b49e73ab5f213
NinaHwang <hwang.ninaa@gmail.com>
2021-12-07 10:27:29 +0900

    Add: .env파일 gitignore에 추가

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom";
-
-import "./index.css";
-import AppRouter from "./Routes";
-// import App from "./App";
-
-ReactDOM.render(<AppRouter />, document.getElementById("root"));
