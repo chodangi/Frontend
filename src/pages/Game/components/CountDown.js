ed791684c4406b4ada2279141c79dafafc1dd291
nooroongy <haerammy@gmail.com>
2022-05-17 01:26:20 +0900

    타이머 생성

--- src/pages/Game/components/CountDown.js
+++ src/pages/Game/components/CountDown.js
@@ -4,14 +4,39 @@ import styled from "styled-components";
 import { size } from "../../../styles/Theme";
 
 const CountDown = () => {
-  const nextGameTime = 6;
-  const minLeft = 23;
-  const secLeft = 57;
+  const [nextGameTime, setNextGameTime] = useState(0)
+  const [minLeft, setMinLeft] = useState(0)
+  const [secLeft, setSecLeft] = useState(0)
+
+  useEffect(() => {
+    const now = new Date()
+    const nowHour = now.getHours();
+    const nowMin = now.getMinutes();
+    const nowSec = now.getSeconds();
+    console.log(nowHour, nowMin)
+
+    setMinLeft(59 - nowMin)
+    setSecLeft(59 - nowSec)
+    setInterval(() => {
+      checkTime()
+    }, 1000);
+  }, [])
+
+  const checkTime = () => [
+    setSecLeft(res => {
+      if (res === 0) {
+        setMinLeft(resMin => resMin - 1)
+        return 59;
+      } else {
+        return res - 1;
+      }
+    })
+  ]
 
   return (
-      <CountDownDiv>
-        <h2>{nextGameTime}시 게임 참여 시간 <span>{minLeft}</span>:<span>{secLeft}</span> 남음</h2>
-      </CountDownDiv>
+    <CountDownDiv>
+      <h2>{nextGameTime}시 게임 참여 시간 <span>{minLeft}</span>:<span>{secLeft}</span> 남음</h2>
+    </CountDownDiv>
   )
 };
 
